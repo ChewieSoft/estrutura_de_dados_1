1551fd7822b36f45b7791790939db350bdbed144
Brenno Sciammarella <brenno.s97@gmail.com>
2022-10-09 11:20:42 -0300

    Tad sem Tad

---
+++ src2/configuration.h
@@ -0,0 +1,9 @@
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void limpar();
+void pausa();
+void sair();
--- src2/main.h
+++ src2/main.h
@@ -1,10 +1,16 @@
-#include "configuration.h"
 
-typedef struct pedido Pedido;
+//PRODUTO
+void incluir_produto();
+void alterar_produto();
+void listar_produtos();
+void consultar_produto();
+void excluir_produto();
 
+//PEDIDO
 void adicionar_produto_carrinho();
 void consultar_carrinho_compras();
 void excluir_produto_carrinho();
 void alterar_quantidade_produto_carrinho();
 void finalizar_pedido();
 void esvaziar_carrinho();
+
