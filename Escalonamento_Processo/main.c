cdd36a9621716f1b28d00c4b99164052341ff67b
Brenno Sciammarella <brenno.s97@gmail.com>
2022-11-06 17:46:21 -0300

    Correcoes e Insercao no ciclo

--- Escalonamento_Processo/main.c
+++ Escalonamento_Processo/main.c
@@ -1,6 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <Windows.h>
+
+#define CYCLE_TIME Sleep(4000)
+
+int cycle = 0;
 
 typedef struct listaProcesso {
 	int qtd;
@@ -18,28 +24,12 @@ typedef struct processo {
 	int proridadeDinamica;
 } Processo;
 
-//Metodos
-ListaProcesso* inicializaListaProcesso(ListaProcesso* lista);
-void carregaDados(ListaProcesso* lista);
 
-int main() {
-
-	ListaProcesso* listaProcesso = NULL;
-	listaProcesso = inicializaListaProcesso(listaProcesso);
-
-	carregaDados(listaProcesso);
-	
-	printf("ASDASD");
-
-	return 0;
-}
-
-ListaProcesso* inicializaListaProcesso(ListaProcesso* lista) {
-	
-	lista = malloc(sizeof(ListaProcesso));
-
+ListaProcesso* inicializaListaProcesso() {	
+	ListaProcesso* lista = malloc(sizeof(ListaProcesso));
 	lista->qtd = 0;
 	lista->processos = NULL;
+	return lista;
 }
 
 void carregaDados(ListaProcesso* lista) {
@@ -79,4 +69,126 @@ void carregaDados(ListaProcesso* lista) {
 		processo = processo_aux;
 	}
 	fclose(arq);
+}
+
+void inserirProcessos(ListaProcesso* listaRaw, ListaProcesso* lista) {
+	Processo* processo = listaRaw->processos;
+	while (processo != NULL) {
+		if (processo->ingresso == cycle) {
+			Processo* aux = malloc(sizeof(Processo));
+			aux->prox = NULL;
+			aux->tarefaId = processo->tarefaId;
+			aux->tempoExecucao = processo->tempoExecucao;
+			aux->tempoRemanescente = processo->tempoRemanescente;
+			aux->ingresso = processo->ingresso;
+			aux->proridadeEstatica = processo->proridadeEstatica;
+			aux->proridadeDinamica = processo->proridadeDinamica;
+			
+			if (lista->processos == NULL) {
+				lista->processos = aux;
+				lista->qtd++;
+			}
+			else {
+				Processo* aux2 = lista->processos;
+				while (aux2->prox != NULL) {
+					aux2 = aux2->prox;
+				}
+				aux2->prox = aux;
+				lista->qtd++;
+			}
+				
+		}
+		processo = processo->prox;
+	}
+}
+
+Processo* selecao(ListaProcesso* lista) {
+	//DEFINIR
+	Processo* aux = lista->processos;
+	Processo* ant = NULL;
+	Processo* retorno = NULL;
+	int prioridade = -1;
+
+	if (lista->qtd == 1) {
+		return lista->processos;
+	}
+
+	while (aux != NULL) {
+		if (aux->proridadeDinamica > prioridade) {
+			prioridade = aux->proridadeDinamica;
+			retorno = aux;
+			ant = aux;
+		}
+		else if (aux->proridadeDinamica == prioridade) {
+			if (ant->tempoRemanescente == aux->tempoRemanescente) {
+				return ant->tarefaId < aux->tarefaId ? ant : aux;
+			}
+			else {
+				return ant->tempoRemanescente < aux->tempoRemanescente ? ant : aux;
+			}
+		}		
+		aux = aux->prox;
+	}
+	return retorno;
+}
+
+void executar(Processo* processo) {
+	processo->tempoRemanescente--;
+}
+
+Processo* excluirTerminado(Processo* anterior, Processo* processo) {
+	anterior->prox = processo->prox;
+	free(processo);
+	return anterior;
+}
+
+void manutencao(Processo* processo, ListaProcesso* lista) {
+	Processo* aux = lista->processos;
+	Processo* ant = NULL;
+	while (aux != NULL) {
+		if (aux != processo) {
+			aux->proridadeDinamica++;
+		}
+		if (aux->tempoRemanescente == 0) {
+			aux = excluirTerminado(ant, aux);
+		}
+		ant = aux;
+		aux = aux->prox;
+	}
+}
+
+void imprimir(ListaProcesso* lista) {
+	
+	Processo* processo = lista->processos;
+	printf("Ciclo: %d\n\n", cycle);
+	printf("Ingresso | P.Id | Tempo Restante | Prioridade Dinamica\n");
+	while (processo != NULL) {
+		printf("%d %d %d %d\n", processo->ingresso, processo->tarefaId, processo->tempoRemanescente, processo->proridadeDinamica);
+		processo = processo->prox;
+	}
+	printf("-----------------------------------------------------------------------------\n");
+}
+
+int main() {
+
+	//Carga dos dados de arquivo
+	ListaProcesso* listaProcessoRaw = inicializaListaProcesso();
+	carregaDados(listaProcessoRaw);
+
+	ListaProcesso* listaProcesso = inicializaListaProcesso();
+
+	int carinha = 1;
+
+	while (carinha) {
+		inserirProcessos(listaProcessoRaw, listaProcesso);
+		Processo* processo = selecao(listaProcesso);
+		executar(processo);
+		manutencao(processo, listaProcesso);
+		imprimir(listaProcesso);
+		//SLEEP PARA DESACELERAR OS CICLOS
+		CYCLE_TIME;
+		cycle++;
+	}
+
+	return 0;
 }
